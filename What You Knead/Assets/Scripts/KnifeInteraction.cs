d44363eb85b1e5b157f3fa8d68a0f7a71c7995b3
Miranda Bisson <mbisson3@gatech.edu>
2021-02-10 19:39:02 -0500

    initial commit

---
+++ What You Knead/Assets/Scripts/KnifeInteraction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeInteraction : MonoBehaviour
+{
+    public ThrowingKnife knife;
+    public float throwSpeed = 10;
+    public int knives = 0;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if you have a knife and right click, throw it
+        //if (Input.GetMouseButtonDown(1) && knives > 0)
+        //{
+        //    Debug.Log("Throwing Knife");
+        //    GameObject cloneKnife = Instantiate(knife, knife.transform.position, knife.transform.rotation) as GameObject;
+        //    cloneKnife.GetComponent<MeshRenderer>().enabled = true;
+        //    cloneKnife.transform.localScale = new Vector3(10, 10, 10);
+        //    cloneKnife.GetComponent<Rigidbody>().AddForce(cloneKnife.transform.forward * throwSpeed);
+        //    knives -= 1;
+        //}
+    }
+
+    void pickUpKnife()
+    {
+        Debug.Log("Picked up Knife");
+        knife.knives++;
+        gameObject.SetActive(false);
+    }
+    
+    void OnMouseOver()
+    {
+        rend.material.color = Color.blue;
+        //if you click and are in the plane pick the knife up
+        if (Input.GetMouseButtonDown(0) && gameObject.tag == "knife")
+        {
+            pickUpKnife();
+        }
+
+    }
+}
+
---
+++ What You Knead/Assets/Scripts/ThrowingKnife.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingKnife : MonoBehaviour
+{
+    public int knives = 0;
+    public int throwSpeed = 5;
+    public GameObject knifeTemplate;
+    Vector3 knifeOffset = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //if you have a knife and right click, throw it
+        if (Input.GetMouseButtonDown(1) && knives > 0)
+        {
+            Debug.Log("Throwing Knife");
+            Vector3 worldOffset = transform.rotation * knifeOffset;
+            GameObject cloneKnife = Instantiate(knifeTemplate, transform.position + worldOffset, transform.rotation) as GameObject;
+            cloneKnife.GetComponent<MeshRenderer>().enabled = true;
+            cloneKnife.transform.localScale = new Vector3(7, 7, 7);
+            cloneKnife.GetComponent<Rigidbody>().AddForce(transform.forward * throwSpeed, ForceMode.Impulse);
+            knives -= 1;
+        }
+    }
+}
