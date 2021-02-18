3af29e11f67b03f2f580aac3d993b685f70c6292
Miranda Bisson <mbisson3@gatech.edu>
2021-02-18 13:45:49 -0500

    Chest

---
+++ What You Knead/Assets/Scripts/ChestOpen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestOpen : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.tag == "Player")
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+}
