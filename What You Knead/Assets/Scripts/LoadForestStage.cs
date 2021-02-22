d62d70faabb1cf4bd706f5f03c320c29fd808e5f
Miranda Bisson <mbisson3@gatech.edu>
2021-02-21 22:14:33 -0500

    Stage 1 base

---
+++ What You Knead/Assets/Scripts/LoadForestStage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadForestStage : MonoBehaviour
+{
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.tag == "Player")
+        {
+            SceneManager.LoadScene("Forest");
+        }
+    }
+}
