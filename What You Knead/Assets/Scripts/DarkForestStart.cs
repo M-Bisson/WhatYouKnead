f0e4ac96356e10e8186e7f6ecea48fa6b8f0f417
Miranda Bisson <mbisson3@gatech.edu>
2021-03-09 11:10:01 -0500

    Forest Stage Update

---
+++ What You Knead/Assets/Scripts/DarkForestStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkForestStart : MonoBehaviour
+{
+    private GameObject character;
+    private Transform knife;
+    public GameObject darkForestCharacter;
+    public ThrowingKnife newKnife;
+    private ThrowingKnife oldKnife;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GameObject.Find("ForestCharacter");
+        knife = character.transform.Find("throwing knife");
+        Debug.Log("Forest boi: " + character);
+        if (character != null)
+        {
+            oldKnife = knife.GetComponent<ThrowingKnife>();
+            Debug.Log("knife??: " + oldKnife);
+            newKnife.knives = oldKnife.knives;
+            character.SetActive(false);
+            Debug.Log("knives from last scene: " + newKnife.knives);
+        }
+    }
+}
---
+++ What You Knead/Assets/Scripts/LoadDarkForest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadDarkForest : MonoBehaviour
+{
+    public GameObject character;
+
+    public void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            DontDestroyOnLoad(character);
+            SceneManager.LoadScene("DarkForest");
+        }
+    }
+}
