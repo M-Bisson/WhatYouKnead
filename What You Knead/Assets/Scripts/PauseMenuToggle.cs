1d56872e1c45c64d3829063477c1f8508d3635f8
annagho <annaho287@gmail.com>
2021-03-18 01:26:24 -0400

    Pause Menu Toggle

---
+++ What You Knead/Assets/Scripts/PauseMenuToggle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class PauseMenuToggle : MonoBehaviour
+{   
+    private CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        //Cursor.lockState = CursorLockMode.None;
+    }
+    void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            if (canvasGroup.interactable)
+            {
+                canvasGroup.interactable = false; canvasGroup.blocksRaycasts = false; canvasGroup.alpha = 0f;
+                Time.timeScale = 1f;
+            }
+            else {
+                canvasGroup.interactable = true; canvasGroup.blocksRaycasts = true; canvasGroup.alpha = 1f;
+                Time.timeScale = 0f; 
+                   
+            }
+        }
+
+    }
+}
