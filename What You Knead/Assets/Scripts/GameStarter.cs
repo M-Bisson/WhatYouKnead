44bb96f0be0f795f578285a816e0ebc61af20bb3
annagho <annaho287@gmail.com>
2021-03-18 01:28:33 -0400

    Game Starter

---
+++ What You Knead/Assets/Scripts/GameStarter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameStarter : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Village" || scene.name == "Menu")
+        {
+            SceneManager.LoadScene("Village");
+        } else if (scene.name == "Forest")
+        {
+            SceneManager.LoadScene("Forest");
+        } else if (scene.name == "DarkForest")
+        {
+            SceneManager.LoadScene("DarkForest");
+        } else if (scene.name == "Field")
+        {
+            SceneManager.LoadScene("Field");
+        }
+    }
+}
