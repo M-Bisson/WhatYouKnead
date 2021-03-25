08a939052c003af7b5cf3a1d4b5a2d5773bf932a
Morgan Powers <morganpowers@Morgans-MacBook-Pro-3.local>
2021-03-25 12:53:35 -0400

    Animate honeycomb

---
+++ What You Knead/Assets/Scripts/Player Interaction/AnimateNearPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateNearPlayer : MonoBehaviour
+{
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            anim.SetBool("PlayerNear", true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            anim.SetBool("PlayerNear", false);
+
+        }
+    }
+}
