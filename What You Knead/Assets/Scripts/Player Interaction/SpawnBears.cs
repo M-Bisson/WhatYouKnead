26686b8d7f5baca2f4c048b9aa55758bcc6d74a8
Miranda Bisson <mbisson3@gatech.edu>
2021-04-06 13:12:57 -0400

    Environment Updates

---
+++ What You Knead/Assets/Scripts/Player Interaction/SpawnBears.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBears : MonoBehaviour
+{
+    public GameObject[] bears = new GameObject[5];
+    public GameObject berry;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject bear in bears)
+        {
+            bear.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        foreach (GameObject bear in bears)
+        {
+            bear.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        foreach (GameObject bear in bears)
+        {
+            if (bear.activeSelf)
+            {
+                break;
+            }
+            berry.SetActive(true);
+        }
+    }
+}
