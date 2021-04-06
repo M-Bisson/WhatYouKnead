3043eff534b34228873d909f5486e34f866aa153
Miranda Bisson <mbisson3@gatech.edu>
2021-04-06 11:11:03 -0400

    Level Design Updates

---
+++ What You Knead/Assets/Scripts/Player Interaction/SpawnGrasshoppers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGrasshoppers : MonoBehaviour
+{
+    public GameObject[] grasshoppers = new GameObject[6];
+    public GameObject wheat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject gh in grasshoppers)
+        {
+            gh.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        foreach (GameObject gh in grasshoppers)
+        {
+            gh.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        foreach (GameObject gh in grasshoppers)
+        {
+            if (gh.activeSelf)
+            {
+                break;
+            }
+            wheat.SetActive(true);
+        }
+    }
+}
