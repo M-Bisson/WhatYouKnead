4047fea559f577eb21449eaccd449718c92e517e
Miranda Bisson <mbisson3@gatech.edu>
2021-04-21 16:44:38 -0400

    character retain attempt

--- What You Knead/Assets/Scripts/Scene Management/ForestStart.cs
+++ What You Knead/Assets/Scripts/Scene Management/ForestStart.cs
@@ -1,28 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ForestStart : MonoBehaviour
 {
     private GameObject character;
-    private Transform knife;
-    public GameObject forestCharacter;
-    public ThrowingKnife newKnife;
-    private ThrowingKnife oldKnife;
     // Start is called before the first frame update
     void Start()
     {
         character = GameObject.Find("VillageCharacter");
-        knife = character.transform.Find("throwing knife");
-        Debug.Log("Village boi: " + character);
-        if (character != null)
-        {
-            oldKnife = knife.GetComponent<ThrowingKnife>();
-            Debug.Log("knife??: " + oldKnife);
-            newKnife.knives = oldKnife.knives;
-            character.SetActive(false);
-            Debug.Log("knives from last scene: " + newKnife.knives);
-        }
+        SceneManager.MoveGameObjectToScene(character, SceneManager.GetActiveScene());
+        Debug.Log("Moved");
+        //character = GameObject.Find("VillageCharacter");
+        //knife = character.transform.Find("throwing knife");
+        //Debug.Log("Village boi: " + character);
+        //if (character != null)
+        //{
+        //    oldKnife = knife.GetComponent<ThrowingKnife>();
+        //    Debug.Log("knife??: " + oldKnife);
+        //    newKnife.knives = oldKnife.knives;
+        //    character.SetActive(false);
+        //    Debug.Log("knives from last scene: " + newKnife.knives);
+        //}
     }
 
 }
--- What You Knead/Assets/Scripts/Scene Management/LoadForestStage.cs
+++ What You Knead/Assets/Scripts/Scene Management/LoadForestStage.cs
@@ -6,12 +6,13 @@ using UnityEngine.SceneManagement;
 public class LoadForestStage : MonoBehaviour
 {
     public GameObject character;
-
+    public GameObject inventory;
     public void OnTriggerEnter(Collider c)
     {
         if(c.tag == "Player")
         {
             DontDestroyOnLoad(character);
+            DontDestroyOnLoad(inventory);
             SceneManager.LoadScene("Forest");
         }
     }
