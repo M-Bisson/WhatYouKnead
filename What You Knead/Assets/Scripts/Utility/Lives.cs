14c19beaa7ee499737bc2bc7788babd608fe2f81
Marissa Yun <marissayun27@gmail.com>
2021-04-23 12:08:31 -0400

    added lives to village scene

---
+++ What You Knead/Assets/Scripts/Utility/Lives.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour
+{
+    public Image[] lives;
+    public int livesRemaining = 5;
+    public Player player;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+        //GameObject[] temp;
+        //GameObject i;
+
+        //while (lives != player.lives)
+        //{
+        //    temp = new GameObject[player.lives];
+        //    livesIcons.CopyTo(temp, 0);
+        //    livesIcons = temp;
+
+        //    i = Instantiate(livesIcons[0]);
+        //    i.transform.SetParent(livesPanel.transform);
+        //    lives++;
+        //}
+
+        while (livesRemaining > player.lives)
+        {
+            livesRemaining--;
+
+            lives[livesRemaining].enabled = false;
+        }
+
+    }
+
+
+}
+
+
