15cfeac70a96b424089a2478ca87c296b28f06f5
jzhou379 <jzhou379@gatech.edu>
2021-04-29 16:00:42 -0400

    fixed nav mesh issues

---
+++ What You Knead/Assets/Scripts/AI/StationaryNPCScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryNPCScript : MonoBehaviour
+{
+
+    public GameObject NPC;
+    public GameObject Player;
+
+    public Canvas dialogueBox;
+    public GameObject button;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(NPC.transform.position, Player.transform.position) < 0.2)
+        {
+            dialogueBox.gameObject.SetActive(true);
+            button.gameObject.SetActive(true);
+        }
+    }
+}
